31dbe946c5ffbd6e217d094e33a555ec741d7cbc
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2024-12-19 13:39:23 -0800

    how about now

--- src/app/api/blogs/[slug]/route.ts
+++ src/app/api/blogs/[slug]/route.ts
@@ -1,26 +1,21 @@
-
 import { NextRequest, NextResponse } from 'next/server'
 import connectDB from '../../../database/db';
 import blogSchema from "../../../database/blogSchema"
 
-type IParams = {
-		params: {
-			slug: string
-		}
+// Typing for params
+type Params = {
+  slug: string;
 }
 
-// If { params } looks confusing, check the note below this code block
-export async function GET(req: NextRequest, { params }: IParams) {
-    await connectDB() // function from db.ts before
-		const { slug } = params // another destructure
-	        const blog = await blogSchema.findOne({ slug }).orFail()
-	        return NextResponse.json(blog)
+// GET API route handler
+export async function GET(req: NextRequest, { params }: { params: Params }) {
+  await connectDB(); // Connect to the database
 
+  const { slug } = params; // Extract slug from params
+
+  // Fetch blog by slug
+  const blog = await blogSchema.findOne({ slug }).orFail(); // Find blog, or fail if not found
+
+  // Return blog data as a JSON response
+  return NextResponse.json(blog);
 }
-
-
-
-
-
-
-
