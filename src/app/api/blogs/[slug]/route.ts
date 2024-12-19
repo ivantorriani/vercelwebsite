0de97971be725955fe1ecd4e5bfe5814244d4d2d
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2024-12-19 14:26:22 -0800

    should be working fingers crossed

--- src/app/api/blogs/[slug]/route.ts
+++ src/app/api/blogs/[slug]/route.ts
@@ -1,21 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '../../../database/db';
-import blogSchema from "../../../database/blogSchema"
+import blogSchema from "../../../database/blogSchema";
 
 // Typing for params
 type Params = {
   slug: string;
-}
+};
 
 // GET API route handler
 export async function GET(req: NextRequest, { params }: { params: Params }) {
-  await connectDB(); // Connect to the database
+  // Connect to the database
+  await connectDB();
 
-  const { slug } = params; // Extract slug from params
+  // Extract slug from params
+  const { slug } = params;
 
   // Fetch blog by slug
-  const blog = await blogSchema.findOne({ slug }).orFail(); // Find blog, or fail if not found
+  
+    const blog = await blogSchema.findOne({ slug }).orFail(); // Find blog or throw an error
 
-  // Return blog data as a JSON response
-  return NextResponse.json(blog);
+    // Return blog data as a JSON response
+    return NextResponse.json(blog);
+
 }
